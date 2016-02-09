baf8c2b084afbb88b2b7e3dc0e1f335e7d797e9f
Ian Hampson <inhampson@gmail.com>
2016-02-08 19:16:28 -0500

    Added the commands to emote, change username, and roll dice.

--- src/server.js
+++ src/server.js
@@ -53,10 +53,63 @@ var onJoined = function(socket) {
 var onMsg = function(socket) {
   socket.on('msgToServer',function(data){
     console.log(data);
-    io.sockets.in('room1').emit('msg',{
-      name:socket.name,
-      msg:data.msg
-    });
+    console.log(data.msg[0]);
+    if(data.msg[0] === '/'){
+      var command = data.msg.split(" ");
+      console.log(command);
+      var message = "";
+      var err = false;
+
+      switch(command[0]){
+        case '/r':
+        case '/R':
+        case '/roll':
+        case '/Roll':
+          message = "You rolled " + command[1] + " " + command[2] + "-sided dice.";
+          for(var i = 0; i< parseInt(command[1]); i++){
+            var result = (1 + Math.floor(Math.random()*parseInt(command[2])));
+            message += result + " ";
+          }
+          break;
+        case '/n':
+        case '/N':
+        case '/name':
+        case '/Name':
+            var newName = command [1];
+            var oldName = socket.name;
+            message = oldName + " changes their name to " + newName + ".";
+            socket.name = newName;
+          break;
+        case '/m':
+        case '/M':
+        case '/me':
+        case '/Me':
+          message += socket.name;
+          for(var i =1;i<command.length;i++){
+            message += command[i];
+          }
+          break;
+        default:
+          message = "That is an invalid command";
+          err = true;
+      }
+      if(err){
+        socket.emit('msg', {name: 'server', msg: message});
+      }else{
+        console.log(message);
+        io.sockets.in('room1').emit('msg',{
+          name:socket.name,
+          msg:message
+        });
+      }
+
+
+    }else{
+      io.sockets.in('room1').emit('msg',{
+        name:socket.name,
+        msg:socket.name + ":" +data.msg
+      });
+    }
   });
 };
 
